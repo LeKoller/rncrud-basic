a1d0ec3ba3705555fcd22a33fa83f671158ba6a6
Leandro Koller <lekoller@yahoo.com>
2021-04-27 16:32:05 -0300

    feat: Finish basics.

--- src/views/UserForm.js
+++ src/views/UserForm.js
@@ -1,6 +1,56 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useState, useContext} from 'react';
+import {Text, TextInput, View, StyleSheet, Button} from 'react-native';
+import UsersContext from '../context/UsersContext';
 
-export default props => {
-  return <Text>UserForm</Text>;
+export default ({route, navigation}) => {
+  const [user, setUser] = useState(route.params ? route.params : {});
+  const {dispatch} = useContext(UsersContext);
+
+  return (
+    <View style={style.form}>
+      <Text>Nome</Text>
+      <TextInput
+        style={style.input}
+        onChangeText={name => setUser({...user, name})}
+        placeholder="Informe o nome"
+        value={user.name}
+      />
+      <Text>Email</Text>
+      <TextInput
+        style={style.input}
+        onChangeText={email => setUser({...user, email})}
+        placeholder="Informe o email"
+        value={user.email}
+      />
+      <Text>URL do avatar</Text>
+      <TextInput
+        style={style.input}
+        onChangeText={avatarUrl => setUser({...user, avatarUrl})}
+        placeholder="Informe a url do avatar"
+        value={user.avatarUrl}
+      />
+      <Button
+        title="Salvar"
+        onPress={() => {
+          dispatch({
+            type: user.id ? 'updateUser' : 'createUser',
+            payload: user,
+          });
+          navigation.goBack();
+        }}
+      />
+    </View>
+  );
 };
+
+const style = StyleSheet.create({
+  form: {
+    padding: 12,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 16,
+  },
+});
--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import {View, Text, FlatList} from 'react-native';
-import {ListItem, Avatar} from 'react-native-elements';
-import users from '../data/users';
+import React, {useContext} from 'react';
+import {View, FlatList, Alert} from 'react-native';
+import {ListItem, Avatar, Button, Icon} from 'react-native-elements';
+import UsersContext from '../context/UsersContext';
 
 export default props => {
+  const {state, dispatch} = useContext(UsersContext);
+
+  function confirmUserDeletion(user) {
+    Alert.alert('Excluir Usuário', 'Deseja excluir usuário?', [
+      {
+        text: 'Sim',
+        onPress() {
+          dispatch({
+            type: 'deleteUser',
+            payload: user,
+          });
+        },
+      },
+      {
+        text: 'Não',
+      },
+    ]);
+  }
+
+  function getAction(user) {
+    return props.navigation.navigate('UserForm', user);
+  }
+
   function getUserItem({item: user}) {
     return (
-      <ListItem
-        key={user.id.toString()}
-        bottomDivider
-        onPress={() => props.navigation.navigate('UserForm')}>
+      <ListItem key={user.id.toString()} bottomDivider>
         <Avatar source={{uri: user.avatarUrl}} />
         <ListItem.Content>
           <ListItem.Title>{user.name}</ListItem.Title>
           <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
-          <Text
-            style={{
-              position: 'absolute',
-              right: 4,
-            }}>
-            Hi stranger
-          </Text>
+          <View style={buttonCaseStyle}>
+            <Button
+              onPress={() => getAction(user)}
+              type="clear"
+              icon={<Icon name="edit" size={25} color="slategray" />}
+            />
+            <Button
+              onPress={() => confirmUserDeletion(user)}
+              type="clear"
+              icon={<Icon name="delete" size={25} color="red" />}
+            />
+          </View>
         </ListItem.Content>
       </ListItem>
     );
@@ -29,10 +54,17 @@ export default props => {
   return (
     <View>
       <FlatList
-        data={users}
+        data={state.users}
         keyExtractor={user => user.id.toString()}
         renderItem={getUserItem}
       />
     </View>
   );
+};
+
+const buttonCaseStyle = {
+  position: 'absolute',
+  right: 0,
+  display: 'flex',
+  flexDirection: 'row',
 };
